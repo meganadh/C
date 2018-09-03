57c6d204f8e817ccc02e66f29794e44d591cbf1c
Meganadha Reddy K <meganadh@gmail.com>
2018-09-03 22:51:06 +0530

    Add files via upload

---
+++ 6.LearnOperators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=6, b=3;
+    printf("\n%d AND %d = %d",a,b,a&b);
+    printf("\n%d OR %d = %d",a,b,a|b);
+    printf("\n%d XOR %d = %d",a,b,a^b);
+
+    printf("\n 5 >> 2 = %d",5>>2);
+
+    getch();
+
+}
---
+++ 6.UnaryOperatorsExample.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+/*******************************************************
+Author : Meganadha Reddy K.
+Purpose : To learn about unary operators
+********************************************************/
+void main()
+{
+    int a=5;
+    //a=a*10;
+    a*=10; //a=a*10;
+    printf("%d",a);
+    getch();
+}
