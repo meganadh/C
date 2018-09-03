81602995a1156e86bac90f3a1477170b864419df
Meganadha Reddy K <meganadh@gmail.com>
2018-09-03 16:36:07 +0530

    Add files via upload

---
+++ 4.a.AddTwoNumbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+/*****************************************************
+* Author : Meganadha Reddy K
+* Purpose : To read two numbers from user and print sum and average
+******************************************************/
+void main()
+{
+    int fn, sn, sum;
+    float average;
+
+    //Read values from user
+    printf("Enter first number:");
+    scanf("%d",&fn);
+    printf("Enter second number:");
+    scanf("%d",&sn);
+
+    //Logic
+    sum=fn+sn;
+    average=sum/2.0;
+
+    printf("Sum = %d, Average=%f",sum,average);
+    getch();
+}
