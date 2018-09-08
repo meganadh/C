27a03021600c7e2c6fe287ee729074b3a58ec5d3
Meganadha Reddy K <meganadh@gmail.com>
2018-09-09 04:02:15 +0530

    Add files via upload

---
+++ 16.continueExample1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+/**************************************************************
+Author : Meganadha Reddy K
+Purpose : To print all numbers divisible by 17 from 1000 to 1500
+          Do not include numbers divisible by 5 in result;
+****************************************************************/
+void main()
+{
+  int i;
+  for(i=1000;i<=1500;i++)
+  {
+      if(i%5==0)
+      {
+          continue;
+      }
+      if(i%17==0)
+      {
+          printf("%d\t",i);
+      }
+  }
+
+}
