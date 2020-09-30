82233b2243d7db0e9ba9bf0bb6e65dff699aadf8
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2020-10-01 00:44:41 +0530

    Update 20.ArraysExample1.c

--- 20.ArraysExample1.c
+++ 20.ArraysExample1.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
+#define MAX 5
 void main()
 {
-    int MAX=3;
+   
 
     int marks[MAX];  //Declaring an array
     int sum=0,i;
-
+    printf("\nEnter number of elements in the array\n");
+    scanf("%d",&n);
     /* Reading Data from user */
-    for(i=0;i<3;i++)
+    printf("\nEnter elements of array\n");
+    for(i=0;i<n;i++)
     {
         printf("Enter any number:");
         scanf("%d",&marks[i]);
     }
 
     /* Finding sum */
-    for(i=0;i<3;i++)
+    for(i=0;i<n;i++)
         sum=sum+marks[i];
 
-    printf("Sum=%d",sum);
+    printf("Sum=%d",sum);//printing sum
     getch();
 }
